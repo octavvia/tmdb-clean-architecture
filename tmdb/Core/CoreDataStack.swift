c1d5c979b866d0c81cd2223f342abae6ffc9e80d
octavia <aulia.octaviani@digitalsekuriti.id>
2024-10-25 13:48:32 +0700

    remove unuse comments - tmdb

--- tmdb/Core/CoreDataStack.swift
+++ tmdb/Core/CoreDataStack.swift
@@ -3,31 +3,6 @@
 //  tmdb
 //
 //  Created by Aulia Octaviani on 25/10/24.
-//
-
-// Data/CoreDataStack.swift
-//
-//import Foundation
-//import CoreData
-//
-//public class CoreDataStack {
-//    public static let shared = CoreDataStack()
-//    
-//    public let persistentContainer: NSPersistentContainer
-//    
-//    private init() {
-//        persistentContainer = NSPersistentContainer(name: "TMDBModel")
-//        persistentContainer.loadPersistentStores { description, error in
-//            if let error = error {
-//                fatalError("Failed to load Core Data stack: \(error)")
-//            }
-//        }
-//    }
-//    
-//    public var context: NSManagedObjectContext {
-//        return persistentContainer.viewContext
-//    }
-//}
 
 // Data/CoreDataStack.swift
 
--- tmdb/Presentation/ViewModels/MovieDetailViewModel.swift
+++ tmdb/Presentation/ViewModels/MovieDetailViewModel.swift
@@ -7,60 +7,6 @@
 
 // Presentation/ViewModels/MovieDetailViewModel.swift
 
-//import Foundation
-//import Combine
-//
-//public class MovieDetailViewModel: ObservableObject {
-//    @Published public var movieDetail: MovieDetail?
-//    @Published public var isLoading = false
-//    @Published public var errorMessage: String?
-//    
-//    private let getMovieDetailUseCase: GetMovieDetailUseCase
-//    private var cancellables = Set<AnyCancellable>()
-//    
-//    public init(getMovieDetailUseCase: GetMovieDetailUseCase) {
-//        self.getMovieDetailUseCase = getMovieDetailUseCase
-//    }
-//    
-//    public func fetchMovieDetail(movieId: Int) {
-//        isLoading = true
-//        errorMessage = nil
-//        getMovieDetailUseCase.execute(movieId: movieId) { [weak self] result in
-//            DispatchQueue.main.async {
-//                self?.isLoading = false
-//                switch result {
-//                case .success(let detail):
-//                    self?.movieDetail = detail
-//                case .failure(let error):
-//                    self?.handleError(error)
-//                }
-//            }
-//        }
-//    }
-//    
-//    private func handleError(_ error: Error) {
-//        if let networkError = error as? NetworkError {
-//            switch networkError {
-//            case .invalidURL:
-//                self.errorMessage = "URL tidak valid."
-//            case .noData:
-//                self.errorMessage = "Tidak ada data yang diterima."
-//            case .decodingError:
-//                self.errorMessage = "Gagal menguraikan data."
-//            case .unknown:
-//                self.errorMessage = "Terjadi kesalahan yang tidak diketahui."
-//            }
-//        } else {
-//            self.errorMessage = error.localizedDescription
-//        }
-//    }
-//}
-//
-
-// Presentation/ViewModels/MovieDetailViewModel.swift
-
-// Presentation/ViewModels/MovieDetailViewModel.swift
-
 import Foundation
 import Combine
 
